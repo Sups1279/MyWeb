2e339421878096ae7d3e906adaeb5a9fb7091e9c
Sups1279 <147906018+Sups1279@users.noreply.github.com>
2023-11-06 07:13:44 +0700

    6/11/2023

--- Nhom4_LTWeb/Controllers/GioHangController.cs
+++ Nhom4_LTWeb/Controllers/GioHangController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nhom4_LTWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,50 @@ namespace Nhom4_LTWeb.Controllers
         {
             return View();
         }
+        public List<GioHang> layGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if(lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+        public ActionResult ThemGioHang(int maSP,string url)
+        {
+            List<GioHang> lstGH = layGioHang();
 
+            GioHang sp = lstGH.Find(n => n.iMasp == maSP);
+
+            if(sp== null)
+            {
+                sp = new GioHang(maSP);
+                lstGH.Add(sp);
+            }
+            else
+            {
+                sp.iSoLuong++;
+            }
+            return Redirect(url);
+        }
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<GioHang> lstioHang = Session["GioHang"] as List<GioHang>;
+            if(lstioHang != null)
+            {
+
+            }
+        }
+        public ActionResult GioHang()
+        {
+            List<GioHang> listGioHang = layGioHang();
+            if (listGioHang.Count()==0)
+            {
+                return RedirectToAction("Index", "SachOnline");
+            }
+            return View(listGioHang);
+        }
     }
 }
