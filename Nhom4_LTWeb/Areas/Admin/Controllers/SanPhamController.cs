9d814ed29e240a6addc89d8da18535fbd41cbb88
NgBaoZ <nqbao123456@gmail.com>
2023-11-12 20:27:28 +0700

    up

---
+++ Nhom4_LTWeb/Areas/Admin/Controllers/SanPhamController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nhom4_LTWeb.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
+
+namespace Nhom4_LTWeb.Areas.Admin.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        DbMyWebDataContext db = new DbMyWebDataContext("Data Source =.; Initial Catalog = ComputerMuda; Integrated Security = True");
+        // GET: Admin/SanPham
+        public ActionResult Index(int? page)
+        {
+            int iPageNum = (page ?? 1);
+            int iPageSize = 7;
+            return View(db.SANPHAMs.ToList().OrderBy(n => n.MaSP).ToPagedList(iPageNum, iPageSize));
+        }
+    }
+}
